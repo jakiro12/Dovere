6bcf6cd150bfd951f975a083de74131e21bc9373
Jakiro12 <eclipsisnocte@gmail.com>
2023-08-06 10:59:44 -0300

    feat camera and permission, it is needs styles

--- components/QrScanner/stylesScanner.js
+++ components/QrScanner/stylesScanner.js
@@ -1,8 +1,33 @@
-import { StyleSheet } from "react-native/types";
+import { StyleSheet } from "react-native";
 
 export default scanerStyles=StyleSheet.create({
     container:{
-        width:100,
-        height:100
+        width:'100%',
+        height:'100%',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    camera:{
+        width:'80%',
+        height:'80%',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    buttonContainer:{
+        width:150,
+        height:40,
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    button:{
+        width:50,
+        height:20,
+
+    },
+    text:{
+        fontSize:16
     }
 })
